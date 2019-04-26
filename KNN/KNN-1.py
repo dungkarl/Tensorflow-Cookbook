4ad2f380581b803882d388a363588ad434ed7d92
dungkarl <dungdinh731989@gmail.com>
2019-04-26 17:29:46 +0700

    26/04/2019

---
+++ KNN/KNN-1.py
@@ -0,0 +1,27 @@
+"""
+    + Working with Nearest Neighbors
+    + Working with Text-Based Distances
+    + Computing Mixed Distance Functions
+    + Using an Address Matching Example
+    + Using Nearest Neighbors for Image Recognition
+"""
+import tensorflow as tf 
+import numpy as np 
+import matplotlib.pyplot as plt 
+import requests
+
+tf.reset_default_graph()
+sess = tf.Session()
+
+
+# prepare data
+
+housing_url = 'https://archive.ics.uci.edu/ml/machine-learningdatabases/housing/housing.data'
+housing_header = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
+cols_used = ['CRIM', 'INDUS', 'NOX', 'RM', 'AGE', 'DIS', 'TAX', 'PTRATIO', 'B', 'LSTAT']
+num_features = len(cols_used)
+# Request data
+housing_file = requests.get(housing_url)
+# Parse Data
+housing_data = [[float(x) for x in y.split(' ') if len(x)>=1] for y in housing_file.text.split('\n') if len(y)>=1]
+
--- SVM/SVM-4.py
+++ SVM/SVM-4.py
@@ -1,41 +1,39 @@
 """
-    + Working with Kernels in TensorFlow
+    + Implementing a Non-Linear SVM
 """
 
 import tensorflow as tf 
 import numpy as np 
 import matplotlib.pyplot as plt 
-from sklearn import datasets
+from sklearn import datasets 
 
 tf.reset_default_graph()
-
 sess = tf.Session()
-# 2: Now we generate the data
-(x_vals, y_vals) = datasets.make_circles(n_samples=500, factor=.5, noise=.1)
+# 2: load data
 
-y_vals = np.array([1 if y==1 else -1 for y in y_vals])
+iris = datasets.load_iris()
+x_vals = np.array([[x[0], x[3]] for x in iris.data])
+y_vals = np.array([1 if y==0 else -1 for y in iris.target])
 class1_x = [x[0] for i,x in enumerate(x_vals) if y_vals[i]==1]
 class1_y = [x[1] for i,x in enumerate(x_vals) if y_vals[i]==1]
 class2_x = [x[0] for i,x in enumerate(x_vals) if y_vals[i]==-1]
 class2_y = [x[1] for i,x in enumerate(x_vals) if y_vals[i]==-1]
+# 3: declare batch size ....
 
-# 3: 
-batch_size = 250
+batch_size = 100
+
 x_data = tf.placeholder(shape=[None, 2], dtype=tf.float32)
-y_target = tf.placeholder(shape=[None, 1], dtype=tf.float32)
+y_target = tf.placeholder(shape=[None,1], dtype=tf.float32)
 prediction_grid = tf.placeholder(shape=[None, 2], dtype=tf.float32)
 b = tf.Variable(tf.random_normal(shape=[1,batch_size]))
-
-# 4: We will now create the Gaussian kernel. This kernel can be expressed as matrix
-# operations as follows:
-
-gamma = tf.constant(-50.0)
+# 4: declare Gaussian Kernel 
+gamma = tf.constant(-30.0)
 dist = tf.reduce_sum(tf.square(x_data), 1)
 dist = tf.reshape(dist, [-1,1])
-sq_dists = tf.add(tf.subtract(dist, tf.multiply(2., tf.matmul(x_data,tf.transpose(x_data)))), tf.transpose(dist))
+sq_dists = tf.add(tf.subtract(dist, tf.multiply(2., tf.matmul(x_data, tf.transpose(x_data)))), tf.transpose(dist))
 my_kernel = tf.exp(tf.multiply(gamma, tf.abs(sq_dists)))
 
-#5: Now we declare the dual problem as previously stated in this recipe
+
 model_output = tf.matmul(b, my_kernel)
 first_term = tf.reduce_sum(b)
 b_vec_cross = tf.matmul(tf.transpose(b), b)
@@ -43,66 +41,55 @@ y_target_cross = tf.matmul(y_target, tf.transpose(y_target))
 second_term = tf.reduce_sum(tf.multiply(my_kernel, tf.multiply(b_vec_cross, y_target_cross)))
 loss = tf.negative(tf.subtract(first_term, second_term))
 
-# 6: 
+# 5: In order to perform predictions using an SVM,
 
 rA = tf.reshape(tf.reduce_sum(tf.square(x_data), 1),[-1,1])
 rB = tf.reshape(tf.reduce_sum(tf.square(prediction_grid), 1),[-1,1])
 pred_sq_dist = tf.add(tf.subtract(rA, tf.multiply(2., tf.matmul(x_data, tf.transpose(prediction_grid)))), tf.transpose(rB))
 pred_kernel = tf.exp(tf.multiply(gamma, tf.abs(pred_sq_dist)))
 prediction_output = tf.matmul(tf.multiply(tf.transpose(y_target),b), pred_kernel)
-prediction = tf.sign(prediction_output - tf.reduce_mean(prediction_output))
+prediction = tf.sign(prediction_output-tf.reduce_mean(prediction_output))
 accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.squeeze(prediction), tf.squeeze(y_target)), tf.float32))
 
-# 7:
+# 6: Next we declare our optimizer function and initialize the variables
 
-my_opt = tf.train.GradientDescentOptimizer(0.001)
+my_opt = tf.train.GradientDescentOptimizer(learning_rate=0.01)
+
 train_step = my_opt.minimize(loss)
 init = tf.global_variables_initializer()
 sess.run(init)
 
-# 8:
+# 7 : training
 loss_vec = []
 batch_accuracy = []
-
-for i in range(500):
+for i in range(300):
     rand_index = np.random.choice(len(x_vals), size=batch_size)
     rand_x = x_vals[rand_index]
     rand_y = np.transpose([y_vals[rand_index]])
     sess.run(train_step, feed_dict={x_data: rand_x, y_target:rand_y})
-    temp_loss = sess.run(loss, feed_dict={x_data:rand_x, y_target:rand_y})
+    temp_loss = sess.run(loss, feed_dict={x_data: rand_x, y_target: rand_y})
     loss_vec.append(temp_loss)
-    acc_temp = sess.run(accuracy, feed_dict={x_data:rand_x, y_target:rand_y, prediction_grid:rand_x})
+    acc_temp = sess.run(accuracy , feed_dict={x_data:rand_x, y_target:rand_y, prediction_grid:rand_x})
     batch_accuracy.append(acc_temp)
-    if (i+1)%100==0:
-        print('Step #' + str(i+1))
-        print('Loss = ' + str(temp_loss))
-        print("Accuracy = " + str(acc_temp))
-    
-# 10:
+
+# 8: we will create a mesh of x, y points and
+# evaluate the prediction function we created on all of these points
 
 x_min, x_max = x_vals[:, 0].min() - 1, x_vals[:, 0].max() + 1
 y_min, y_max = x_vals[:, 1].min() - 1, x_vals[:, 1].max() + 1
 xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.02), np.arange(y_min, y_max, 0.02))
-grid_points = np.c_[xx.ravel(), yy.ravel()]
-[grid_predictions] = sess.run(prediction, feed_dict={x_data:rand_x, y_target:rand_y, prediction_grid:grid_points})
+grid_points = np.c_[xx.ravel(), yy.ravel()] # chuyển thành mảng 2D
+[grid_predictions] = sess.run(prediction, feed_dict={x_data:rand_x,y_target:rand_y,prediction_grid: grid_points})
 grid_predictions = grid_predictions.reshape(xx.shape)
 
-# 11: visualize data 
-plt.contourf(xx, yy, grid_predictions, cmap=plt.cm.get_cmap("Greys"), alpha=0.8)
-plt.plot(class1_x, class1_y, 'ro', label='Class 1')
-plt.plot(class2_x, class2_y, 'kx', label='Class -1')
+# 9: 
+plt.contourf(xx, yy, grid_predictions, cmap=plt.cm.Paired, alpha=0.8)
+plt.plot(class1_x, class1_y, 'ro', label='I. setosa')
+plt.plot(class2_x, class2_y, 'kx', label='Non setosa')
+plt.title('Gaussian SVM Results on Iris Data')
+plt.xlabel('Pedal Length')
+plt.ylabel('Sepal Width')
 plt.legend(loc='lower right')
-plt.ylim([-1.5, 1.5])
-plt.xlim([-1.5, 1.5])
-plt.show()
-plt.plot(batch_accuracy, 'k-', label='Accuracy')
-plt.title('Batch Accuracy')
-plt.xlabel('Generation')
-plt.ylabel('Accuracy')
-plt.legend(loc='lower right')
-plt.show()
-plt.plot(loss_vec, 'k-')
-plt.title('Loss per Generation')
-plt.xlabel('Generation')
-plt.ylabel('Loss')
+plt.ylim([-0.5, 3.0])
+plt.xlim([3.5, 8.5])
 plt.show()
